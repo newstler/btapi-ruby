d0bbb9505f91ab6aecd4eb4404d1eb4cf2e2325d
Yuri Sidorov <yurisidorov@evrone.com>
2022-05-09 02:49:03 +0200

    1.0.23

--- lib/bullet_train_test.rb
+++ lib/bullet_train_test.rb
@@ -41,8 +41,8 @@ require 'bullet_train_test/models/api_v1_team_serializer_data_attributes'
 require 'bullet_train_test/models/api_v1_team_serializer_data_relationships'
 
 # APIs
+require 'bullet_train_test/api/_api'
 require 'bullet_train_test/api/goals_api'
-require 'bullet_train_test/api/n_api'
 require 'bullet_train_test/api/projects_api'
 require 'bullet_train_test/api/public_api'
 require 'bullet_train_test/api/teams_api'
